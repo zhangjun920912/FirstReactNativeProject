5e2b06ef81d17c0969358e8bd0a554c5ec3f7a66
Zhangjunimust <Zhangjunimst@gmail.com>
2017-03-17 18:32:35 +0800

    init project

---
+++ FirstReactNativeProject/www/HomePage/homepage.js
@@ -0,0 +1,25 @@
+/**
+ * Created by zhangjunimust on 17/3/17.
+ */
+import React,{Component} from 'react'
+import {
+    View,
+    Text,
+    Image,
+} from 'react-native'
+
+export default class HomePage extends Component
+{
+    constructor(props)
+    {
+        super(props);
+    }
+
+    render()
+    {
+        return(
+            <View>
+                <Text>zhangjun</Text>
+            </View>);
+    }
+}
